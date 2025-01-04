1be341c1b1f1f6f785342a34777ad51e78acf8b4
hyper <revoluer9@gmail.com>
2025-01-04 21:03:35 +0900

    concat, 상수 넣기 concat은 String 타입만 넣을 수 있어서 stringValue 이용 enum 사용 시 참고할 것

--- src/test/java/jpabook/querydsl/QuerydslBasicTest.java
+++ src/test/java/jpabook/querydsl/QuerydslBasicTest.java
@@ -2,7 +2,9 @@ package jpabook.querydsl;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Expression;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.JPQLQueryFactory;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -420,4 +422,33 @@ public class QuerydslBasicTest {
         }
     }
 
+    /**
+     * 상수
+     */
+    @Test
+    public void constant() {
+        List<Tuple> result = queryFactory.select(member.username, Expressions.constant("A"))
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("result: " + tuple);
+        }
+    }
+
+    /**
+     * 문자 더하기
+     */
+    @Test
+    public void concat() {
+        List<String> result = queryFactory
+                .select(member.username.concat("_").concat(member.age.stringValue())) //concat String만 쓸 수 있어서, 해당 부분 참고 (enum)
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("result: " + s);
+        }
+    }
 }
