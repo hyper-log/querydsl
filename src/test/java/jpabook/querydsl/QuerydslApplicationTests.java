24dfd81ce5a3a9e5efbfec8f7b199ced5b0165f1
hyper <revoluer9@gmail.com>
2025-01-08 17:14:59 +0900

    조회 API 컨트롤러 개발

--- src/test/java/jpabook/querydsl/QuerydslApplicationTests.java
+++ src/test/java/jpabook/querydsl/QuerydslApplicationTests.java
@@ -69,7 +69,9 @@ class QuerydslApplicationTests {
         condition.setAgeLoe(40);
         condition.setTeamName("teamB");
 
-        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+        List<MemberTeamDto> result = memberJpaRepository.search(condition);
         assertThat(result).extracting("username").containsExactly("member4");
     }
+
+
 }
